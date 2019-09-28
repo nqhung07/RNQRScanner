e8ee383772156a3fc31bf77633304dea3369adb7
hungnguyen <hung.nguyen@wesport.zone>
2019-09-28 23:58:07 +0700

    history display item

--- src/screens/History.js
+++ src/screens/History.js
@@ -1,14 +1,19 @@
 /* eslint-disable prettier/prettier */
 import React, {Component} from 'react';
-import {Text, View, TouchableOpacity} from 'react-native';
+import {Text, View, TouchableOpacity, FlatList, StyleSheet} from 'react-native';
 import AsyncStorage from '@react-native-community/async-storage';
 
 export default class History extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      dataSource: [],
+    };
   }
   componentDidMount() {
-    // this.getDataStore();
+    this.getDataStore();
+    console.log("dataSource: ", this.state.dataSource);
+    
   }
   getDataStore = async () => {
     try {
@@ -16,7 +21,9 @@ export default class History extends Component {
       const currentValue = await AsyncStorage.getItem('scandata');
       const currentValueObj = JSON.parse(currentValue);
       console.log('currentValue:', currentValueObj);
-      return currentValueObj;
+
+      this.setState({dataSource: currentValueObj}) ;
+
     } catch {
       console.log('get data error');
     }
@@ -24,27 +31,55 @@ export default class History extends Component {
   removeValue = async () => {
     try {
       await AsyncStorage.removeItem('scandata');
+      this.setState({dataSource: []}) ;
       console.log('Remove done');
+
     } catch (e) {
       console.log('Remove err');
     }
   };
+
+  renderItem = item => {
+        return (
+            <TouchableOpacity
+              style={styles.flatView}
+              onPress={ () => {} }>
+              <Text style={styles.itemName}>Item: {item.data}</Text>
+            </TouchableOpacity>
+          );
+      }
+
   render() {
     return (
       <View>
-        <TouchableOpacity
-          onPress={() => {
-            this.getDataStore();
-          }}>
-          <Text>Show Items</Text>
-        </TouchableOpacity>
         <TouchableOpacity
           onPress={() => {
             this.removeValue();
           }}>
           <Text>Remove Items</Text>
         </TouchableOpacity>
+        <FlatList
+          data={this.state.dataSource}
+          renderItem={({item}) => this.renderItem(item)}
+
+        />
+        
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: 'skyblue',
+    },
+    flatView: {
+        borderBottomColor: "steelblue",
+        borderBottomWidth: 2,
+        padding: 15
+    },
+    itemName: {
+        fontSize: 18,
+    },
+});
