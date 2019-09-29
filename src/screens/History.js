6d7d2bf6f2c119bab502b51b4f237478820940a7
hungnguyen <hung.nguyen@wesport.zone>
2019-09-29 16:39:03 +0700

    Ask agree to delete hostory data in store

--- src/screens/History.js
+++ src/screens/History.js
@@ -1,6 +1,14 @@
 /* eslint-disable prettier/prettier */
 import React, {Component} from 'react';
-import {Text, View, TouchableOpacity, FlatList, StyleSheet, SafeAreaView} from 'react-native';
+import {
+  Text,
+  View,
+  TouchableOpacity,
+  FlatList,
+  StyleSheet,
+  SafeAreaView,
+  Alert
+} from 'react-native';
 import AsyncStorage from '@react-native-community/async-storage';
 import Header from './header';
 
@@ -27,6 +35,22 @@ export default class History extends Component {
       console.log('get data error');
     }
   };
+  onRemoveData() {
+    // Works on both iOS and Android
+    Alert.alert(
+      'Delete all history data',
+      'Click Ok to Delete, Cancel to exit',
+      [
+        {
+          text: 'Cancel',
+          onPress: () => console.log('Cancel Pressed'),
+          style: 'cancel',
+        },
+        {text: 'OK', onPress: () => this.removeValue()},
+      ],
+      {cancelable: false},
+    );
+  }
   removeValue = async () => {
     try {
       await AsyncStorage.removeItem('scandata');
@@ -53,14 +77,14 @@ export default class History extends Component {
   render() {
     var key = 0;
     return (
-      <SafeAreaView style ={styles.container}>
+      <SafeAreaView style={styles.container}>
         <Header
           navigation={this.props.navigation}
           title="History"
           rightIcon="trash"
           color="white"
-          background = "steelblue"
-          rightAction = {()=>this.removeValue()}
+          background="steelblue"
+          rightAction={() => this.onRemoveData()}
         />
         <FlatList
           data={this.state.dataSource}
@@ -77,8 +101,8 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   flatView: {
-    paddingHorizontal:5,
-    paddingVertical:10,
+    paddingHorizontal: 5,
+    paddingVertical: 10,
     marginHorizontal: 5,
     marginVertical: 2,
     backgroundColor: 'skyblue',
